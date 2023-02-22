4423511557b070173b848f283943773953eab707
mayoreee <mayoreee1@gmail.com>
2023-02-22 04:31:05 +0100

    fixes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
-import * as dotenv from 'dotenv';
-dotenv.config()
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const PORT = 8080;
 const HOST = "localhost";
@@ -10,13 +10,15 @@ const API_SERVICE_URL = process.env.API_SERVICE_URL;
 const app = express();
 
 // GET Info endpoint
-app.get("/info", (req, res, next) => {
-  res.send("This is a proxy service which proxies to JMES RPC.");
+app.get("/", (req, res, next) => {
+  res
+    .sendStatus(200)
+    .send("This is a proxy service which proxies to JMES RPC.");
 });
 
 // Proxy endpoints
 app.use(
-  "/",
+  "/rpc/",
   createProxyMiddleware({
     target: API_SERVICE_URL,
     changeOrigin: true,
